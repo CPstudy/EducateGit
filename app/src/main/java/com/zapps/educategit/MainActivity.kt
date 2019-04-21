a506a8c5abc7c8afc8d1f674fe04ab87a7180a33
CPstudy <guide94@naver.com>
2019-04-21 14:31:00 +0900

    add SecondActivity

--- app/src/main/java/com/zapps/educategit/MainActivity.kt
+++ app/src/main/java/com/zapps/educategit/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.zapps.educategit
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun moveActivity(v: View) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
