b56c1e96c31da2ff565c950746fa9cc8203dff57
zimin.byun <wlals822@naver.com>
2019-04-21 14:57:44 +0900

    add onclick image

--- app/src/main/java/com/zapps/educategit/SecondActivity.kt
+++ app/src/main/java/com/zapps/educategit/SecondActivity.kt
@@ -2,11 +2,20 @@ package com.zapps.educategit
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        imageView.setOnClickListener(::onImageViewClick)
+    }
+
+    fun onImageViewClick(view: View) {
+        Toast.makeText(this, "티모 이미지를 터치하였습니다.", Toast.LENGTH_SHORT).show()
     }
 }
