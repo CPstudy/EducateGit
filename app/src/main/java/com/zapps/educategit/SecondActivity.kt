d241670b23c17c02f4ac2df86421125a070e48d2
CPstudy <guide94@naver.com>
2019-04-21 14:59:22 +0900

    can click Teemo

--- app/src/main/java/com/zapps/educategit/SecondActivity.kt
+++ app/src/main/java/com/zapps/educategit/SecondActivity.kt
@@ -2,11 +2,19 @@ package com.zapps.educategit
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        imageView.setOnClickListener {
+            Log.d("Teemo", "Hello Teemo")
+        }
     }
 }
